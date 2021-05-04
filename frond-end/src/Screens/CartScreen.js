ad99d2f74d43f24c5b0e4ccf082b1057cbd9a468
Yann-Devs <elfomenprofessor@gmail.com>
2021-05-04 08:21:12 -0400

    Modifying the cart item

--- frond-end/src/Screens/CartScreen.js
+++ frond-end/src/Screens/CartScreen.js
@@ -1,9 +1,25 @@
 import './CartScreen.css'
-
+import Cart from '../components/cartItem'
 function CartScreen() {
     return (
         <div className="cartScreen">
-            <h1>The Cart Screen</h1>
+            <div className="cartscreen__left">
+                <h2>The Shopping cart</h2>
+                <Cart />
+            </div>
+            <div className="cartscreen__rigth">
+                <div className="cartitem__info">
+                    <p>
+                        Sub total (0) items
+                    </p>
+                    <p>
+                        499.99$
+                    </p>
+                    <p>
+                        <button>Proceed to Checkout</button>
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ frond-end/src/components/cartItem.js
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom'
+import './CartItem.css'
+function cartItem() {
+    return (
+        <div className="cartitem">
+            <div className="cartitem__image">
+                <img src="https://cdn.cultofmac.com/wp-content/uploads/2016/10/Hello-Again-New-MacBook-Pro.jpg"/>
+            </div>
+
+            <Link to={`/product/${1111}`} className="cartitem__name">
+                <p>Product 1</p>
+            </Link>
+            <p className="cartitem__price">
+                499.99$
+            </p>
+            <select className="cartitem__select">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+            </select>
+
+            <button className="cartitem__delete">
+                <i className="fas fa-trash"></i>
+            </button>
+        </div>
+    )
+}
+
+export default cartItem
